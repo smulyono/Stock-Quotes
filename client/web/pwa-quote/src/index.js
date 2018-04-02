4fb831a2e957f8f8e7b1633402de2814ee23275b
Sanny Mulyono <smulyono@me.com>
2018-04-01 23:00:40 -0700

    added prettier in the config

--- client/web/pwa-quote/src/index.js
+++ client/web/pwa-quote/src/index.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import Dom from 'react-dom';
-import Header from './components/header';
-import CardLists from './components/cardLists';
+import React from "react";
+import Dom from "react-dom";
+import Header from "./components/header";
+import CardLists from "./components/cardLists";
 
-import { AppProvider } from './context/stockContext';
+import { AppProvider } from "./context/stockContext";
 
 /* importing styles from `./assets/styles/index.css */
 import "./assets/styles/index.less";
 
 // /* Start of javascript */
 Dom.render(
-    <AppProvider>
-        <Header />
-        <CardLists />
-    </AppProvider>,
-    document.getElementById("root")
+  <AppProvider>
+    <Header />
+    <CardLists />
+  </AppProvider>,
+  document.getElementById("root")
 );
 
 // hot module reload
 if (module.hot) {
-    module.hot.accept();
+  module.hot.accept();
 }
